00a1f10c0f4c331a9e51936edcc92dc548ed792f
Haniki <117754210+Haniki2k4@users.noreply.github.com>
2024-10-24 23:02:02 +0700

    modified

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('clients.home', ['name'=>'Nguyen Van A']);
+})->name('home ');
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
